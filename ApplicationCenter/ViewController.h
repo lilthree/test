792b3e98857f44055c5950838349ac497a22b410
lilthree <87437904@qq.com>
2015-12-30 16:29:57 +0800

    test

---
+++ ApplicationCenter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ApplicationCenter
+//
+//  Created by zhaothree on 15/12/30.
+//  Copyright © 2015年 zhaothree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ApplicationCenter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ApplicationCenter
+//
+//  Created by zhaothree on 15/12/30.
+//  Copyright © 2015年 zhaothree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
